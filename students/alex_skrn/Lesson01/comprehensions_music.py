ea335a03faec522f68a191ca696847fa88ce76ce
AlexSkrn <askrn123@gmail.com>
2018-04-05 22:25:33 +0300

    Added some time measurements

--- students/alex_skrn/Lesson01/comprehensions_music.py
+++ students/alex_skrn/Lesson01/comprehensions_music.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Solutions to Lesson01 comprehensions assignment re music."""
 import pandas as pd
+# import time
 
 # Instructions summary:
 # 1) get artists and song names for tracks with danceability scores over 0.8
@@ -12,7 +13,9 @@ import pandas as pd
 music = pd.read_csv("featuresdf.csv")
 
 # Three versions of solution -- all results are identical
+
 # INITIAL SOLUTION -- with indixing
+# start_time1 = time.time()
 res1 = sorted(
               [
                (music.name[i],
@@ -25,7 +28,8 @@ res1 = sorted(
               key=lambda x: x[2],
               reverse=True
               )[0:5]
-
+# mytime1 = time.time() - start_time1
+# print("Elapsed time = ", mytime1)
 print(res1)
 # prints
 # [('Bad and Boujee (feat. Lil Uzi Vert)', 'Migos', 0.93, -5.31),
@@ -34,8 +38,10 @@ print(res1)
 #  ("You Don't Know Me - Radio Edit", 'Jax Jones', 0.88, -6.05),
 #  ('Bank Account', '21 Savage', 0.88, -8.23)]
 
+
 # SECOND SOLUTION -- with list comprehension and zip - THIS MUST BE IT
 # n, a, d, l are for name, artist, danceability, and loudness
+# start_time2 = time.time()
 res2 = sorted(
               [
                (n, a, round(d, 2), round(l, 2))
@@ -49,7 +55,8 @@ res2 = sorted(
               key=lambda x: x[2],
               reverse=True
               )[0:5]
-
+# mytime2 = time.time() - start_time2
+# print("Elapsed time = ", mytime2)
 print(res2)
 # prints
 # [('Bad and Boujee (feat. Lil Uzi Vert)', 'Migos', 0.93, -5.31),
@@ -58,23 +65,26 @@ print(res2)
 #  ("You Don't Know Me - Radio Edit", 'Jax Jones', 0.88, -6.05),
 #  ('Bank Account', '21 Savage', 0.88, -8.23)]
 
+
 # Solution No. 3 -- with generator expression instead of list comprehension
-# Probably this is the best one.
 # n, a, d, l are for name, artist, danceability, and loudness
+# Not sure if this solution is any better, if I measure the time correctly.
+# start_time3 = time.time()
 res3 = sorted(
               (
                (n, a, round(d, 2), round(l, 2))
-               for n, a, d, l
-               in zip(music.name,
-                      music.artists,
-                      music.danceability,
-                      music.loudness)
+               for n, a, d, l in zip(music.name,
+                                     music.artists,
+                                     music.danceability,
+                                     music.loudness
+                                     )
                if d > 0.8 and l < -5.0
                ),
               key=lambda x: x[2],
               reverse=True
               )[0:5]
-
+# mytime3 = time.time() - start_time3
+# print("Elapsed time = ", mytime3)
 print(res3)
 # prints
 # [('Bad and Boujee (feat. Lil Uzi Vert)', 'Migos', 0.93, -5.31),
