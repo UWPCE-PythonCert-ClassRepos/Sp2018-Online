35fbef88ae023c5b7c12dd8a77c0148c5bed00a2
Chay Casso <chay@alum.mit.edu>
2018-04-09 20:50:49 -0700

    Initial solutions for generator.

--- students/Chay_Casso/Lesson01/generator_solution.py
+++ students/Chay_Casso/Lesson01/generator_solution.py
@@ -4,21 +4,36 @@
 # Chay Casso
 # 4/9/2018
 
+from itertools import *
+
+
 def intsum():
-    pass
+    return accumulate(count())
 
 
 def intsum2():
-    pass
+    return accumulate(count())
 
 
 def doubler():
-    pass
+    return map(lambda x: 2**x, count())
 
+def fibonacci(n):
+    """Return the nth fibonacci sequence number."""
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 def fib():
-    pass
+    return map(lambda n: fibonacci(n), count(2))
 
+def prime_run(x):
+    for i in range(2, x):
+        if x % i == 0: return False
+    else: return True
 
 def prime():
-    pass
+    return filter(lambda x: prime_run(x), count(2))
