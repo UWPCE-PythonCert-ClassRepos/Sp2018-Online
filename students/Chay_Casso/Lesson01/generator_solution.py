6008f576e5d4a5cd14d1ea911ce2763e9947e571
Chay Casso <chay@alum.mit.edu>
2018-04-09 19:48:15 -0700

    Initial framework for commit.

---
+++ students/Chay_Casso/Lesson01/generator_solution.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#
+# Generators
+# Chay Casso
+# 4/9/2018
+
+def intsum():
+    pass
+
+
+def intsum2():
+    pass
+
+
+def doubler():
+    pass
+
+
+def fib():
+    pass
+
+
+def prime():
+    pass
