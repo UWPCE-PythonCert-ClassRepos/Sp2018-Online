67e7c1d3789aed4daab1b924097d5e7ca180392b
Darrell Hart <dahart@adobe.com>
2018-04-11 09:02:58 -0700

    removed duplicated functions

--- students/darrell/wk1 - assignments/generator_solution.py
+++ students/darrell/wk1 - assignments/generator_solution.py
@@ -20,35 +20,6 @@ def doubler():
             yield n
         n *= 2
 
-def fib():
-    seq = [1]
-    while True:
-        yield seq[-1]
-        if len(seq) == 1:
-            seq.append(1)
-        else:
-            # add the last 2 numbers in seq and append
-            seq.append(sum(seq[-2:]))
-
-
-def intsum():
-    seq =[0]
-    while True:
-        if len(seq) == 1:
-            yield 0
-        else:
-            yield sum(seq)
-        seq.append(seq[-1] + 1)
-
-
-def doubler():
-    n = 1
-    while True:
-        if n == 1:
-            yield 1
-        else:
-            yield n
-        n *= 2
 
 def fib():
     seq = [1]
@@ -59,6 +30,7 @@ def fib():
         else:
             # add the last 2 numbers in seq and append
             seq.append(sum(seq[-2:]))
+
 
 def prime():
     prime_numbers = []
@@ -74,11 +46,10 @@ def is_prime(n):
     prime_num = False
     if n in [2, 3, 5, 7]:
         return True
-    for i in range(2,n):
+    for i in range(2, n):
         if n % i == 0:
             prime_num = False
             break
         else:
             prime_num = True
     return prime_num
-
