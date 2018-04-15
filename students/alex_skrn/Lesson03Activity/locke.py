510277a05aa27f83ff909ab9d9c9ad064eb91474
AlexSkrn <askrn123@gmail.com>
2018-04-15 19:44:27 +0300

    Added comments and return True to exit method

--- students/alex_skrn/Lesson03Activity/locke.py
+++ students/alex_skrn/Lesson03Activity/locke.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Lesson 03 Activity -- Context Manager."""
 
+# I don't know if the __exit__ method should return True or allow the Exception
+#  raised by the move_boats_through() method propagate.
+
 
 class Locke(object):
     """Very simple model of a locke operation."""
@@ -20,7 +23,7 @@ class Locke(object):
     def move_boats_through(self, boats_num):
         """Do nothing but raise ValueError if arg boats exceeds limit."""
         if boats_num > self.num:
-            raise ValueError
+            raise ValueError("Too many boats")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         """Do nothing but print several statements."""
@@ -28,3 +31,4 @@ class Locke(object):
         print("Opening the doors.")
         print("Closing the doors.")
         print("Restarting the pumps.")
+        # return True # This handles the exception - but should it be handled?
