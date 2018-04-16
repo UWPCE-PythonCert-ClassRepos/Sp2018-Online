a56c103680451db0923cb5a0d077d83e58ce024f
Scott Luse <scottwluse@gmail.com>
2018-04-15 20:40:23 -0700

    Updated test_generator to work with generator_1 functions

--- students/ScottL/lesson01/test_generator.py
+++ students/ScottL/lesson01/test_generator.py
@@ -20,21 +20,12 @@ def test_intsum():
     assert next(g) == 10
     assert next(g) == 15
 
-'''This test case is not used in generator_1 assignment
 
-def test_intsum2():
-
-    #this test case  
-    g = gen.intsum2()
-
-    assert next(g) == 0
-    assert next(g) == 1
-    assert next(g) == 3
-    assert next(g) == 6
-    assert next(g) == 10
-    assert next(g) == 15
-    
 '''
+This test case is not used in generator_1 assignment
+def test_intsum2(): 
+'''
+
 
 def test_doubler():
 
@@ -62,3 +53,26 @@ def test_prime():
     g = gen.prime()
     for val in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]:
         assert next(g) == val
+
+
+def main():
+    print("\n"+"1. Sum of integers:")
+    print("==============================")
+    test_intsum()
+
+    print("\n"+"2. Double the previous value:")
+    print("==============================")
+    test_doubler()
+
+    print("\n"+"3. Fibonacci sequence:")
+    print("==============================")
+    test_fib()
+
+    print("\n"+"4. Prime numbers:")
+    print("==============================")
+    test_prime()
+
+
+if __name__ == "__main__":
+    main()
+
