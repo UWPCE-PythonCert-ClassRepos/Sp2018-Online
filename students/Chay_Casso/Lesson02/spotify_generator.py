67fd39d9c94f4fda9c43e0e0dbb1ce176380c2e5
Chay Casso <chay@alum.mit.edu>
2018-04-16 19:30:42 -0700

    Solution for the generator.

--- students/Chay_Casso/Lesson02/spotify_generator.py
+++ students/Chay_Casso/Lesson02/spotify_generator.py
@@ -2,7 +2,17 @@
 #
 # Spotify Generator
 # Chay Casso, 4/15/18
+#
+# Ed Sheeran songs:
+# "Shape of You"
+# "Castle on the Hill"
+# "Galway Girl"
+# "Perfect"
+
 
 import pandas as pd
 
 music = pd.read_csv("featuresdf.csv")
+ed_songs = filter((lambda x: x[1] == "Ed Sheeran"), zip(music.name, music.artists))
+solution = list(ed_songs)
+print(solution)
