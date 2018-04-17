91fc1ee64437cba8467290f2a1218ab323492801
Darrell Hart <dahart@adobe.com>
2018-04-17 11:36:50 -0700

    added filter alternative syntax to track finder

--- students/darrell/wk2 - assignments/high_energy_closure.py
+++ students/darrell/wk2 - assignments/high_energy_closure.py
@@ -4,9 +4,11 @@ import pandas as pd
 def energetic_track_finder(min_energy_level=0.8):
     music = pd.read_csv("featuresdf.csv")
     def get_tracks():
-        nonlocal music, min_energy_level
+        # nonlocal music, min_energy_level
         zipped_data = zip(music.artists, music.name, music.energy)
-        filtered_list = [song for song in zipped_data if song[2] > min_energy_level]
+        # filtered_list = [song for song in zipped_data if song[2] > min_energy_level]
+        # either of these work
+        filtered_list = list(filter(lambda song: song[2] > min_energy_level, zipped_data))
         return filtered_list
     return get_tracks
 
