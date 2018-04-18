b6f09da7daff9fa9d7a2504144813122956d8fd0
PreethiKrishnan11 <preethi.krishnan11@gmail.com>
2018-04-17 18:52:50 -0700

    Please review and provide feedback for generators file. I have changed it as per your comments

--- students/PreethiKrishnan/lesson_01/generators.py
+++ students/PreethiKrishnan/lesson_01/generators.py
@@ -1,12 +1,18 @@
 #Method to find sum of 2 integers
-def sum_of_integers(len_num):
+def intsum(len_num):
     n = 0
     final_list = []
-    for i in range(len_num):
-        #print("This is the list number {}".format(i))
-        n += i
-        final_list.append(n)
-    print("This is the sum list: {}".format(final_list))
+    if (len_num == 0 or len_num == 1):
+        yield n
+        #print(n)
+    else:
+        for i in range(len_num):
+            #print("This is the list number {}".format(i))
+            n += i
+            yield n
+            #final_list.append(n)
+    #print(n)
+    #print("This is the sum list: {}".format(final_list))
 
 #Method for doubler
 def doubler(len_num):
@@ -15,25 +21,26 @@ def doubler(len_num):
     for i in range(1, len_num):
         if(i == 1):
             final_list.append(i)
-            #print("This is the list number {}".format(i))
-            #print("This is doubler: {}".format(i))
+            next_value = i
         else:
             next_value = previous_value*2
-            #print("This is doubler: {}".format(next_value))
             final_list.append(next_value)
             previous_value = next_value
+    yield next_value
+    #print(next_value)
     print("This is doubler list: {}".format(final_list))
 
 #Method for fibonacci
-def fibonacci_series(len_num):
+def fib(len_num):
     series = [1,1]
     for i in range(len_num):
         new_num = series[i] + series[i+1]
         series.append(new_num)
+    yield series
     print("This is the fibonacci list: {}".format(series))
 
 #Method for Prime numbers
-def prime_numbers(len_num):
+def prime(len_num):
     #print("entering prime")
     prime_list = []
     for i in range(2, len_num):
@@ -45,11 +52,12 @@ def prime_numbers(len_num):
             count += 1
         if(new_count == 1):
             prime_list.append(i)
-            #print(prime_list)
+    yield prime_list
     print("This is the list of prime numbers: {}".format(prime_list))
 
-
-sum_of_integers(10)
-doubler(10)
-fibonacci_series(10)
-prime_numbers(10)
+if __name__ == '__main__':
+    print(intsum(3))
+    print(intsum(0))
+    print(doubler(2))
+    print(fib(10))
+    print(prime(10))
