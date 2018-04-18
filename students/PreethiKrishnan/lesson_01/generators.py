217ab96b99a1136603a3102238a9411fb54a0ac2
PreethiKrishnan11 <preethi.krishnan11@gmail.com>
2018-04-17 21:47:46 -0700

    Please review generators code. I submitted the previous one by mistake. It was a wrong file.Please provide feedback

--- students/PreethiKrishnan/lesson_01/generators.py
+++ students/PreethiKrishnan/lesson_01/generators.py
@@ -1,49 +1,47 @@
+import itertools
+
 #Method to find sum of 2 integers
-def intsum(len_num):
-    n = 0
-    final_list = []
-    if (len_num == 0 or len_num == 1):
-        yield n
-        #print(n)
-    else:
-        for i in range(len_num):
-            #print("This is the list number {}".format(i))
-            n += i
-            yield n
-            #final_list.append(n)
-    #print(n)
-    #print("This is the sum list: {}".format(final_list))
+def intsum():
+    sum = 0
+    num = 0
+    while True:
+        yield sum
+        num += 1
+        sum += num
+
+#Method to find sum of 2 integers
+def intsum2():
+    sum = 0
+    num = 0
+    while True:
+        yield sum
+        num += 1
+        sum += num
+
 
 #Method for doubler
-def doubler(len_num):
+def doubler():
     previous_value = 1
-    final_list = []
-    for i in range(1, len_num):
-        if(i == 1):
-            final_list.append(i)
-            next_value = i
-        else:
-            next_value = previous_value*2
-            final_list.append(next_value)
-            previous_value = next_value
-    yield next_value
-    #print(next_value)
-    print("This is doubler list: {}".format(final_list))
+    current_value = 1
+    while True:
+        yield current_value
+        previous_value += 1
+        current_value = previous_value*2
 
 #Method for fibonacci
-def fib(len_num):
-    series = [1,1]
-    for i in range(len_num):
-        new_num = series[i] + series[i+1]
-        series.append(new_num)
-    yield series
-    print("This is the fibonacci list: {}".format(series))
+def fib():
+    first_num, second_num = 1, 1
+    while True:
+        yield first_num
+        new_num = first_num + second_num
+        first_num = second_num
+        second_num = new_num
 
 #Method for Prime numbers
-def prime(len_num):
-    #print("entering prime")
+def prime_numbers():
     prime_list = []
-    for i in range(2, len_num):
+    len_num = itertools.count(start=2, step=1)
+    for i in len_num:
         count = 2
         new_count = 0
         while(count <= i):
@@ -52,12 +50,6 @@ def prime(len_num):
             count += 1
         if(new_count == 1):
             prime_list.append(i)
-    yield prime_list
-    print("This is the list of prime numbers: {}".format(prime_list))
+            yield i
 
-if __name__ == '__main__':
-    print(intsum(3))
-    print(intsum(0))
-    print(doubler(2))
-    print(fib(10))
-    print(prime(10))
+
