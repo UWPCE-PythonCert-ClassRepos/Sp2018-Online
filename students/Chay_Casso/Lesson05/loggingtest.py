484a5e24e7260ccb92457e98c4301a356efc9307
Chay Casso <chay@alum.mit.edu>
2018-05-06 20:14:14 -0700

    Added syslog, unsure if it works.

--- students/Chay_Casso/Lesson05/loggingtest.py
+++ students/Chay_Casso/Lesson05/loggingtest.py
@@ -1,10 +1,14 @@
 #simple.py
 import datetime
 import logging
+import logging.handlers
 
 format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+syslogformat = "%(filename)s:%(lineno)-3d %(levelname)s %(message)s"
 
 formatter = logging.Formatter(format)
+syslogformatter = logging.Formatter(syslogformat)
+
 now = datetime.datetime.now()
 
 current_date = "%d-%d-%d" % (now.year, now.month, now.day)
@@ -15,6 +19,10 @@ file_handler.setFormatter(formatter)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(formatter)
+
+syslog_handler = logging.handlers.DatagramHandler("127.0.0.1", 514)
+syslog_handler.setLevel(logging.ERROR)
+syslog_handler.setFormatter(syslogformatter)
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
