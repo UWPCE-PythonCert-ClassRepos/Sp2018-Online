8c30030b399e587cfaab4d0a4851c682c25a56a2
Marc Charbonneau <mtl2bk@gmail.com>
2018-05-20 11:51:49 -0700

    module 6

---
+++ students/marc_charbo/assignment_6/activity_6/calculator/exceptions.py
@@ -0,0 +1,6 @@
+"""
+This module provides basic error handling when an operation is attempted on less than 2 operands
+"""
+class InsufficientOperands(Exception):
+    """ insufficient operands class"""
+    pass
