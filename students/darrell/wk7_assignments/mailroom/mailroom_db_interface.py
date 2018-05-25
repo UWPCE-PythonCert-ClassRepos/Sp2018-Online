954457da99453fed168578415e00c00de30d60d1
Darrell Hart <dahart@adobe.com>
2018-05-25 16:30:09 -0700

    adding initial mailroom code.  create and seed database

---
+++ students/darrell/wk7_assignments/mailroom/mailroom_db_interface.py
@@ -0,0 +1,18 @@
+import logging
+from peewee import *
+from mailroom_schema import Donor, Donation
+
+
+def create_donor():
+    pass
+
+def read_donors():
+    pass
+
+def update_donor():
+    pass
+
+def delete_donor():
+    pass
+
+
---
+++ students/darrell/wk7_assignments/mailroom/mailroom_schema.py
@@ -0,0 +1,41 @@
+import logging
+from peewee import *
+import datetime
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+logger.info('Creating Mailroom Database')
+database = SqliteDatabase('../data/mailroom.db')
+database.connect()
+database.execute_sql('PRAGMA foreign_keys = ON;') # needed for sqlite only
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+class Donor(BaseModel):
+    """
+        This class defines Donor.
+    """
+    logger.info('Building Donor Class')
+    donor_first_name = CharField(primary_key= True, max_length= 30)
+    donor_last_name = CharField( max_length = 30)
+    donor_city= CharField( max_length = 30)
+
+class Donation(BaseModel):
+    """
+        This is the Donation class, Because we have not specified a primary key,
+        peewee will automatically add an auto-incrementing integer primary
+        key field named id
+    """
+    logger.info('Building Donation Class')
+    created_date = DateTimeField(default=datetime.datetime.now)
+    donation_amount = DecimalField(auto_round= False, max_digits=10, decimal_places=2)
+    donor = ForeignKeyField(Donor,related_name='donated_by', null=False)
+
+
+
+database.create_tables([Donor, Donation])
+database.close()
